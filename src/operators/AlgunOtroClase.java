58000249efa012c491ba68c701ac352c1ed7448d
menro <menro@localhost>
2024-04-30 09:53:43 +0200

    Creado el Start y el paquete operators con sus clases

---
+++ src/operators/AlgunOtroClase.java
@@ -0,0 +1,12 @@
+package operators;
+
+import data.Cliente;
+
+public class AlgunOtroClase {
+
+	public void setDireccion(Cliente cliente, String nuevaDireccion) {
+		cliente.setDireccion(nuevaDireccion);
+		// Otro código aquí
+	}
+
+}
---
+++ src/operators/OtraClaseMas.java
@@ -0,0 +1,11 @@
+package operators;
+
+import data.Cliente;
+
+public class OtraClaseMas {
+
+	public void enviarCorreo(Cliente cliente) {
+		String direccion = cliente.getDireccion();
+		// Lógica para enviar correo a la dirección
+	}
+}
